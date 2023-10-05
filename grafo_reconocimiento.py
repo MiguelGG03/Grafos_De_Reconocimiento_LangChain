b57680510b5c32cc757d3462e9bc086d47db329e
migueliiin <91721552+MiguelGG03@users.noreply.github.com>
2023-10-05 19:01:23 +0200

    lo de los nodos lo tengo, pero ahora falta lo dif

--- grafo_reconocimiento.py
+++ grafo_reconocimiento.py
@@ -6,20 +6,19 @@ G = nx.DiGraph()
 
 # Agregar nodos (países, capitales y vecinos)
 data = [
-    {"country": "USA", "capital": "Washington, D.C.", "neighbors": ["Canada", "Mexico"]},
-    {"country": "Canada", "capital": "Ottawa", "neighbors": ["USA"]},
-    {"country": "Mexico", "capital": "Mexico City", "neighbors": ["USA", "Guatemala"]},
-    {"country": "Guatemala", "capital": "Guatemala City", "neighbors": ["Mexico", "Belize", "Honduras"]},
-    {"country": "Belize", "capital": "Belmopan", "neighbors": ["Guatemala", "Mexico"]},
-    {"country": "Honduras", "capital": "Tegucigalpa", "neighbors": ["Guatemala", "Nicaragua", "El Salvador"]},
-    {"country": "Nicaragua", "capital": "Managua", "neighbors": ["Honduras", "Costa Rica"]},
-    {"country": "El Salvador", "capital": "San Salvador", "neighbors": ["Honduras"]},
-    {"country": "Costa Rica", "capital": "San Jose", "neighbors": ["Nicaragua", "Panama"]}
+    {"country": "USA", "capital": "Washington, D.C.", "population": 328.2, "currency": "USD", "neighbors": ["Canada", "Mexico"]},
+    {"country": "Canada", "capital": "Ottawa", "population": 37.6, "currency": "CAD", "neighbors": ["USA"]},
+    {"country": "Mexico", "capital": "Mexico City", "population": 126.2, "currency": "MXN", "neighbors": ["USA", "Guatemala"]},
+    {"country": "Guatemala", "capital": "Guatemala City", "population": 17.2, "currency": "GTQ", "neighbors": ["Mexico", "Belize", "Honduras"]},
+    {"country": "Belize", "capital": "Belmopan", "population": 0.4, "currency": "BZD", "neighbors": ["Guatemala", "Mexico"]},
+    {"country": "Honduras", "capital": "Tegucigalpa", "population": 9.7, "currency": "HNL", "neighbors": ["Guatemala", "Nicaragua", "El Salvador"]},
+    {"country": "Nicaragua", "capital": "Managua", "population": 6.5, "currency": "NIO", "neighbors": ["Honduras", "Costa Rica"]},
+    {"country": "El Salvador", "capital": "San Salvador", "population": 6.4, "currency": "USD", "neighbors": ["Honduras"]},
+    {"country": "Costa Rica", "capital": "San Jose", "population": 5.1, "currency": "CRC", "neighbors": ["Nicaragua", "Panama"]}
 ]
-
 # Agregar nodos y relaciones
 for item in data:
-    G.add_node(item["country"], type="country")
+    G.add_node(item["country"], type="country", population=item["population"], currency=item["currency"])
     G.add_node(item["capital"], type="capital")
     G.add_edge(item["country"], item["capital"])
 
@@ -30,7 +29,7 @@ for item in data:
 pos = nx.spring_layout(G, seed=42)
 node_labels = {node: node for node in G.nodes()}
 nx.draw(G, pos, with_labels=True, labels=node_labels, node_size=800, node_color="skyblue", font_size=10, font_weight="bold")
-edge_labels = {(country, neighbor): neighbor for country, neighbor in G.edges() if G.nodes[neighbor]["type"] == "country"}
+edge_labels = {edge: f"{G.nodes[edge[0]]['currency']} {G.nodes[edge[0]]['population']}M" for edge in G.edges() if "type" in G.nodes[edge[1]] and G.nodes[edge[1]]["type"] == "country"}
 nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_color="red")
 
 # Muestra el gráfico
