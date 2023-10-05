8b9868ae4566241e549d5df50fe748470ef2f68e
migueliiin <91721552+MiguelGG03@users.noreply.github.com>
2023-10-05 18:49:19 +0200

    uwu?

--- grafo_reconocimiento.py
+++ grafo_reconocimiento.py
@@ -1,4 +1,5 @@
 import networkx as nx
+import matplotlib.pyplot as plt
 
 # Crear un grafo dirigido
 G = nx.DiGraph()
@@ -9,3 +10,11 @@ capitals = ["Washington, D.C.", "Ottawa", "Mexico City"]
 
 for country, capital in zip(countries, capitals):
     G.add_edge(country, capital)
+
+# Dibuja el grafo
+pos = nx.circular_layout(G)
+nx.draw(G, pos, with_labels=True, node_size=800, node_color="skyblue", font_size=10, font_weight="bold")
+
+# Muestra el gráfico
+plt.axis("off")
+plt.show()
